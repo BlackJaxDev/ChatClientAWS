bd6872eb6a459fb73dacf011b2f32a4d9e5d9df4
David Eddy <dneddy@comcast.net>
2019-05-13 18:48:39 -0700

    Started setting up database API get/post routes

--- Server/server.js
+++ Server/server.js
@@ -17,27 +17,52 @@ app.set('port', port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var schema = new mongoose.Schema(
-{
+var MessageModel = mongoose.model('Message', {
     name : String,
     message : String
 });
-var MessageModel = mongoose.model('Message', schema);
-
+var UserModel = mongoose.model('User', {
+    name : String,
+    id : Number,
+    notifCount : Number,
+});
+var ServerChannelModel = mongoose.model('Server', {
+    serverName : String,
+    channelName : String,
+});
+//example.com/server/settings/cool
+app.get('/api/server/settings/:name', function(req, res)
+{
+    const params = req.params; //params = {name:"cool"}
+    var serverName = params.name;
+    ServerChannelModel.find({ serverName: serverName, channelName: channelName });
+})
+//example.com/server/cool/best_channel
+app.get('/api/server/:serverName/:channelName', function(req, res)
+{
+    const params = req.params; //params = {serverName:"cool", channelName:"best_channel"}
+    var serverName = params.serverName;
+    var channelName = params.channelName;
+    ServerChannelModel.find({ serverName: serverName, channelName: channelName });
+})
 //example.com/user/bob?id=2265
-app.get('/user/:name', function(req, res)
+app.get('/api/user/:name', function(req, res)
 {
     const query = req.query; //query = {id:"2265"}
     const params = req.params; //params = {name:"bob"}
+    var userName = params.name;
+    var userID = query.id;
+    UserModel.find({ name: userName, id: userID });
 })
-app.get('/api/messages', (req, res) =>
+app.get('/api/messages/:user/:otherUser', (req, res) =>
 {
+    //Return only the messages from this user to the other, not the other's messages to this user
     MessageModel.find({ }, (err, messages) => 
     {
         res.send(messages);
     })
 })
-app.post('/api/messages', async (req, res) => 
+app.post('/api/messages/:user/:otherUser', async (req, res) => 
 {
     try
     {
@@ -68,6 +93,7 @@ io.on('connection', function(socket)
 });
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+
 const db = mongoose.connection;  
 db.on('error', () =>
 {
