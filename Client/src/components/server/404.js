be3b9b833199a9676bc284b099886ec8bf040324
David Eddy <dneddy@comcast.net>
2019-06-01 03:44:44 -0700

    Updated 404 page, changed global font color

--- Client/src/components/server/404.js
+++ Client/src/components/server/404.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./404.css";
+import { Link } from 'react-router-dom';
+import { Image } from 'semantic-ui-react';
+import img404 from '../../../public/images/common/404.png';
 
 class Page404 extends React.Component 
-{  
+{
   constructor(props) 
   {
     super(props);
@@ -10,8 +13,10 @@ class Page404 extends React.Component
   render() 
   {
     return (
-      <div className="component-404">
-      404
+      <div className="center-screen">
+      <Image src={img404} size='small' />
+      <h1>404.</h1>
+      <h3>We couldn't find what you were looking for. <Link to="/">Back to home.</Link></h3>
       </div>
     );
   }
--- Client/src/logic/WithFirebase.js
+++ Client/src/logic/WithFirebase.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import FirebaseContext from './FirebaseApp';
 
 export const WithFirebase = Component => props => (
