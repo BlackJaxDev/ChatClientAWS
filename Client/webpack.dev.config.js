c73c16f159daa3ebd4a9661c250c657c4c6096a7
David Eddy <dneddy@comcast.net>
2019-06-01 03:43:51 -0700

    Minor config changes

--- Client/webpack.dev.config.js
+++ Client/webpack.dev.config.js
@@ -130,7 +130,7 @@ module.exports =
                 ]
             },
             {
-                test: /\.(png|jpg)$/,
+                test: /\.(png|jpg|svg|gif|woff|woff2|eot|ttf|otf)$/,
                 use: 
                 [
                     { 
@@ -140,6 +140,20 @@ module.exports =
                             limit: 8192,
                         } 
                     },
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: 
+                [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: 
+                [
+                    'xml-loader'
                 ]
             }
         ]
